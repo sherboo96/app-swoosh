672617677c799000f5fc7fa4b6fa9903cb5f29cd
Sherbeny <sherbeny2096@gmail.com>
2020-05-19 14:09:06 +0200

    Make Change

---
+++ app-swoosh/app-swoosh/View/ButtonBorder.swift
@@ -0,0 +1,21 @@
+//
+//  ButtonBorder.swift
+//  app-swoosh
+//
+//  Created by Mahmoud Sherbeny on 5/19/20.
+//  Copyright © 2020 Mahmoud Sherbeny. All rights reserved.
+//
+
+import UIKit
+
+class ButtonBorder: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
