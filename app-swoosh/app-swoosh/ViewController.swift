78a5ee859e3c4e07e99e4d0d632c05ae3130bac5
Sherbeny <sherbeny2096@gmail.com>
2020-05-19 14:38:05 +0200

    Segue unWind

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bgImg: UIImageView!
+    @IBOutlet weak var swooshImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        swooshImg.frame = CGRect(x: view.frame.size.width / 2 - swooshImg.frame.size.width / 2, y: 50, width: swooshImg.frame.size.width, height: swooshImg.frame.size.height)
+//
+//        bgImg.frame = view.frame
+    }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
     }
 
 
