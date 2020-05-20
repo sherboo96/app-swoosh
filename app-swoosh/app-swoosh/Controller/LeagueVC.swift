1f89413a58e4814604b37aca4be60f723f9b6fe0
Sherbeny <sherbeny2096@gmail.com>
2020-05-20 12:50:24 +0200

    Final Commit

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,13 +10,39 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: ButtonBorder!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
     
-
+    
+    @IBAction func onMenTabbed(_ sender: Any) {
+        selectLeague(leagueString: "Men")
+    }
+    
+    @IBAction func onWommanTabbed(_ sender: Any) {
+        selectLeague(leagueString: "Womman")
+    }
+    
+    @IBAction func onCoedTabbed(_ sender: Any) {
+        selectLeague(leagueString: "Coed")
+    }
+    
+    func selectLeague(leagueString: String){
+        player.desiredLeague = leagueString
+        nextBtn.isEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
     
     @IBAction func nextButtonAC(_ sender: Any) {
         performSegue(withIdentifier: "SkillVCSegue", sender: self)
